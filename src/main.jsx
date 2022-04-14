6d81cb1c8d895df6f0d5a1791dffb1bb6d17f3e4
Koen Haagsma <koen.haagsma@themobilecompany.com>
2022-04-14 11:13:46 +0200

    Edit version and delete not used css link

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Main } from './Pages/index';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
